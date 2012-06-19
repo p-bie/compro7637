2b452465f250bbb8f6eb6b85d25f2d339e337c9b
Pariwas Sittichan <p.bie@hotmail.com>
2012-06-19 14:39:55 +0700

    เพิ่มProgramming is fun

--- src/th/ac/nu/student/u54347637/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54347637/compro/ch01/HelloWorld.java
@@ -6,5 +6,6 @@ package th.ac.nu.student.u54347637.compro.ch01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Programming is fun");
     }
 }
