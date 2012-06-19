c043c643fcf91ab92cb760d816c5dfd9a69e0c3b
Pariwas Sittichan <p.bie@hotmail.com>
2012-06-19 14:18:57 +0700

    สร้างHelloWorld

---
+++ src/th/ac/nu/student/u54347637/compro/ch01/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347637.compro.ch01;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
