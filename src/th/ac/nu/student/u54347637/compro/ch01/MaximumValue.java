f69cdf1ad33fff6ef6bbd0de70654bd14cbcab90
Pariwas Sittichan <p.bie@hotmail.com>
2012-07-11 19:53:11 +0700

    บทที่ 2

---
+++ src/th/ac/nu/student/u54347637/compro/ch01/MaximumValue.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347637.compro.ch01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MaximumValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num = 2147483647;
+        System.out.println(num);   // 
+        System.out.println(num+1); 
+        int num2 = -2147483648;
+      System.out.println(num2);   //
+      System.out.println(num2-1);   // 
+        // TODO code application logic here
+    }
+}
